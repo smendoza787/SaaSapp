b987da025a90443f93c740a5a327cd5ef8057728
Sergio Mendoza <smendoza787@gmail.com>
2017-03-07 12:01:18 -0800

    added Plans table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Plan.create(name: 'Basic', price: 0)
+Plan.create(name: 'Pro', price: 10)
